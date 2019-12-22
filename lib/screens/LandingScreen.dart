660217df2f3bac999667df34d55e512b32ac747c
EYoung75 <eyoung75.ey@gmail.com>
2019-12-21 20:51:19 -0600

    added act screen

--- lib/screens/LandingScreen.dart
+++ lib/screens/LandingScreen.dart
@@ -3,7 +3,7 @@ import "../widgets/customAppBar.dart";
 
 class LandingScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
